1c0107830065759a82fe5dc439dcff73201a7439
mirco991 <mirco.azzolini99@gmail.com>
2025-04-01 12:47:50 +0200

    Delete imposta.py

--- imposta.py
+++
@@ -1,6 +0,0 @@
-reddito = input("Quale è il tuo reddito?")
-red=int(reddito)
-
-if red<28000 : print("La tua imposta è: ", 28000/100*23)
-if 28000<red<50000 : print("La tua imposta è: ", (28000/100*23)+(22000/100*35))
-##if red>50000 : print("La tua imposta è: ",(red)/100*43)
