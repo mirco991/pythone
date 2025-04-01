a2258eadcbea8d5888aa4f4ea7a9f1cfb76c8409
mirco991 <mirco.azzolini99@gmail.com>
2025-04-01 15:45:48 +0200

    Update albero.py

--- albero.py
+++ albero.py
@@ -3,5 +3,5 @@ ampiezza = int(input("Quanto deve essere grande l'ampiezza dell'albero?"))
 
 i= 1
 while i <= ampiezza :
-    print(" " * ((ampiezza-i)//2)+carattere*i)
+    print(" " * ((ampiezza-i)//2)+carattere*(i+1-(ampiezza%2)))
     i+=2
